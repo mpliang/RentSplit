73dd9564ecd262c6d2d65063b3e7ae9bc4a8f8ff
Michael Liang <michaelpliang@gmail.com>
2015-12-01 15:42:18 -0800

    initial commit

---
+++ frontend/www/js/controllers/HomeCtrl.js
@@ -0,0 +1,61 @@
+app.controller('HomeCtrl', function($scope, $stateParams , Profiles) {
+	$scope.bills = [
+    {
+      type: 'rent',
+      amount: 3500,
+      due: '12/23/15',
+      roomates: [
+        'John',
+        'Steve',
+        'Ben',
+      ],
+      notes: 'no notes',
+      repeat: 'monthly',
+			paid: false,
+    },
+    {
+      type: 'internet/cable',
+      amount: 200,
+      due: '12/15/15',
+      roomates: [
+        'John',
+        'Ben',
+      ],
+      notes: 'no notes',
+      repeat: 'monthly',
+			paid: false,
+    },
+    {
+      type: 'water',
+      amount: 100,
+      due: '12/23/15',
+      roomates: [
+        'John',
+        'Steve',
+        'Ben',
+      ],
+      notes: 'no notes',
+      repeat: 'monthly',
+			paid: false,
+    },
+    {
+      type: 'paid',
+      amount: 100,
+      due: '12/23/15',
+      roomates: [
+        'John',
+        'Steve',
+        'Ben',
+      ],
+      notes: 'no notes',
+      repeat: 'monthly',
+			paid: false,
+    },
+  ];
+
+
+
+	$scope.paid = (bill) => {
+		bill.paid = !bill.paid;
+	}
+});
---
+++ frontend/www/js/module.js
@@ -0,0 +1,3 @@
+window.app = angular.module('rentSplit', ['ionic', 'starter.controllers', 'starter.services'])
+// window.url = 'https://pokemon-shelter.herokuazpp.com/';
+// window.url = 'http://localhost:3000/';
