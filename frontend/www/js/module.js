7148bf22a12ed8501727f3aa8dff19f3e2114cf5
Michael Liang <michaelpliang@gmail.com>
2015-12-27 23:59:23 -0800

    added new page

--- frontend/www/js/module.js
+++ frontend/www/js/module.js
@@ -1,3 +1,3 @@
 window.app = angular.module('rentSplit', ['ionic', 'starter.controllers', 'starter.services', 'ngOpenFB'])
-// window.url = 'https://pokemon-shelter.herokuazpp.com/';
+// window.url = 'https://pokemon-shelter.herokuapp.com/';
 // window.url = 'http://localhost:3000/';
